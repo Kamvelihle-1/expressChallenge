e0a97e7b0b67d0b67fc730def978394d58bb2c40
SmasherK <ksikalan@gmail.com>
2023-08-14 15:54:32 +0200

    Main entry index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,20 @@
-//index
+const {express,routes} = require('./controller')
+const app = express()
+const path = require('path')
+const port = +process.env.PORT || 3000
+
+//static
+app.use(express.static('./static'))
+app.use(
+    express.urlencoded({
+        extended:false
+    }),
+    routes
+)
+routes.get('^/$|?challenger',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./static/html/index.html'))
+})
+
+app.listen(port,()=>{
+    console.log(`The server is running on port ${port}`);
+})
